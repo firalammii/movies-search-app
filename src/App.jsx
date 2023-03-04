ec01aa68718153dae513795ce02694fd48c4cdc9
firalammii <90164296+firalammii@users.noreply.github.com>
2023-03-04 21:17:43 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css';
+import SeachBar from './components/SeachBar';
+
+function App () {
+
+  return (
+    <div className="App">
+      <h1 className='title' >Movies Search App</h1>
+      <SeachBar />
+    </div>
+  );
+}
+
+export default App;
